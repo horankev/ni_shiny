9f0298973c212f1bbfacb2e78ef1fc3db570db35
horankev <horank@gmail.com>
2025-10-28 20:35:56 +0000

    further colour scales

--- app.R
+++ app.R
@@ -212,7 +212,7 @@ server <- function(input, output, session) {
       } 
       else if (input$variable == "protestant_pct") {
         fill_scale <- tm_scale_continuous(
-          values = c("lightsteelblue1", "steelblue", "darkblue")
+          values = c("gray80", "darkblue")
         )
       } 
       else {
