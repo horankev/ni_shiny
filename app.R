546d54ca68eff5b9157220e96576c4b3dfbee90a
horankev <horank@gmail.com>
2025-10-28 20:31:32 +0000

    further colours

--- app.R
+++ app.R
@@ -212,7 +212,7 @@ server <- function(input, output, session) {
       } 
       else if (input$variable == "protestant_pct") {
         fill_scale <- tm_scale_continuous(
-          values = c("lightblue", "darkblue")
+          values = c("mintcream", "darkblue")
         )
       } 
       else {
